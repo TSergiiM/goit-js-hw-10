6b747d6f1d9944ec6ff3291e8980669f5d95240f
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2022-01-02 17:32:17 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import './css/styles.css';
 
+const debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
+
+const refs = { input: document.querySelector('#search-box') };
+refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+//функція зчитує інпут юзера
+function onInput(event) {
+  const name = event.target.value;
+  console.log(name);
+  fetchCountries(name);
+}
+function fetchCountries(name) {
+  return fetch(`https://restcountries.com/v3.1/name/${name}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      // Data handling
+    })
+    .catch(error => {
+      // Error handling
+    });
+}
