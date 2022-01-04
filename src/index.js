3e350d2a1076945043b9db4f0fa72ff872061f4f
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2022-01-04 22:59:41 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './css/styles.css';
+import { Notify } from 'notiflix';
 import { fetchCountries } from './fetchCountries';
 
 const debounce = require('lodash.debounce');
@@ -7,24 +8,45 @@ const DEBOUNCE_DELAY = 300;
 const refs = {
   input: document.querySelector('#search-box'),
   cardConteiner: document.querySelector('.country-info'),
+  countryList: document.querySelector('.country-list'),
 };
 refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 function onInput(event) {
   const contryName = event.target.value;
   // console.log(contryName);
-  fetchCountries(contryName.trim()).then(renderCountryCard);
   if (contryName === '') {
     return;
   }
+  fetchCountries(contryName.trim()).then(renderCountryCard).catch(nonExistentCountry);
 }
 
-//розмітка карточки країни
+//------------розмітка карточки країни
 function renderCountryCard(response) {
-  // console.log(response);
-  const markup = response
-    .map(({ flags, name, population, capital, languages }) => {
-      return `
+  console.log(response);
+  // console.log(response.length);
+  //------------якщо знайдено >10 то повідомлення
+  if (response.length > 10) {
+    refs.countryList.innerHTML = '';
+    Notify.info('Too many matches found. Please enter a more specific name.');
+    // ------------якщо знайдено >2 то рендеримо список буде прапор-країна
+  } else if (response.length >= 2) {
+    const markup = response
+      .map(({ flags, name }) => {
+        return `
+  <h1>
+  <img src=${flags.svg} width="65" height="40" alt= ${name.common}> 
+  ${name.official}</h1>`;
+      })
+      .join('');
+    // console.log(markup);
+    refs.cardConteiner.innerHTML = '';
+    refs.countryList.innerHTML = markup;
+    //------------якщо знайдено 1 то редеримо карточку країни
+  } else {
+    const markup = response
+      .map(({ flags, name, population, capital, languages }) => {
+        return `
   <h1>
   <img src=${flags.svg} width="65" height="40" alt= ${name.common}> 
   ${name.official}</h1>
@@ -33,8 +55,16 @@ function renderCountryCard(response) {
   <li><b>Population:</b> ${population}</li>
   <li><b>Languages:</b> ${Object.values(languages)}</li
   ></ul>`;
-    })
-    .join('');
-  // console.log(markup);
-  refs.cardConteiner.innerHTML = markup;
+      })
+      .join('');
+    // console.log(markup);
+    refs.countryList.innerHTML = '';
+    refs.cardConteiner.innerHTML = markup;
+  }
+}
+//------------опрацьовуємо неіснуючу країну
+function nonExistentCountry() {
+  Notify.failure('Oops, there is no country with that name');
+  refs.countryList.innerHTML = '';
+  refs.cardConteiner.innerHTML = '';
 }
