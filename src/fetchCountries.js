dfd3a10d733f28f5d48eb9b28c69c94b8dda77be
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2022-01-04 21:13:07 +0200

    fix-funk-fetchCountries

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,11 @@
-function fetchCountries(name) {
-  return fetch(`https://restcountries.com/v3.1/name/${name}`)
+function fetchCountries(contryName) {
+  const searchParams = new URLSearchParams({
+    fields: 'capital,flags,languages,name,population',
+  });
+
+  const url = `https://restcountries.com/v3.1/name/${contryName}?${searchParams}`;
+
+  return fetch(url)
     .then(response => {
       if (!response.ok) {
         throw new Error(response.status);
